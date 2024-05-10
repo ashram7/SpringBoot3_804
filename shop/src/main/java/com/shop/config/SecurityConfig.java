1c7e751e6068146316786367fe4cfe73bb468445
ashram7 <ashram7@hanmail.net>
2024-05-10 10:18:10 +0900

    보안로그인

--- shop/src/main/java/com/shop/config/SecurityConfig.java
+++ shop/src/main/java/com/shop/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.shop.config;
 
+import com.shop.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -7,16 +9,32 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+    @Autowired
+    MemberService memberService;
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.formLogin(form -> form
+                .loginPage("/members/login") //로그인 페이지 URL 설정
+                .defaultSuccessUrl("/") //로그인 성공 시 이동할 URL 설정
+                .usernameParameter("email") //로그인 시 사용할 파라미터 이름으로 email을 지정
+                .failureUrl("/members/login/error") //로그인 실패 시 이동할 URL을 설정
+        )
+        .logout(logout -> logout
+                //로그 아웃 URL을 설정
+                .logoutRequestMatcher(new AntPathRequestMatcher("/members/logout"))
+                .logoutSuccessUrl("/") //로그아웃 성공 시 이동할 URL을 설정
+        );
         return http.build();
     }
     @Bean
-    public PasswordEncoder passwordEncoder() {
+    public PasswordEncoder passwordEncoder() { //비밀번호 암호화를 위해 passwordEncoder를 지정
+
         return new BCryptPasswordEncoder();
     }
 }
--- shop/src/main/java/com/shop/controller/MemberController.java
+++ shop/src/main/java/com/shop/controller/MemberController.java
@@ -3,10 +3,12 @@ package com.shop.controller;
 import com.shop.dto.MemberFormDto;
 import com.shop.entity.Member;
 import com.shop.service.MemberService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,11 +27,31 @@ public class MemberController {
     }
 
     @PostMapping(value = "/new")
-    public String newMember(MemberFormDto memberFormDto){
+    public String newMember(@Valid MemberFormDto memberFormDto, BindingResult bindingResult, Model model){
+        if(bindingResult.hasErrors()){
+            return "member/memberForm";
+        }
 
-        Member member = Member.createMember(memberFormDto, passwordEncoder);
-        memberService.saveMember(member);
+        try {
+            Member member = Member.createMember(memberFormDto, passwordEncoder);
+            memberService.saveMember(member);
+        } catch (IllegalStateException e){
+            model.addAttribute("errorMessage", e.getMessage());
+            return "member/memberForm";
+        }
 
         return "redirect:/";
     }
+
+    @GetMapping(value = "/login")
+    public String loginMember(){
+        return "/member/memberLoginForm";
+    }
+
+    @GetMapping(value = "/login/error")
+    public String loginError(Model model){
+        model.addAttribute("loginErrorMsg", "아이디 또는 비밀번호를 확인해주세요");
+        return "/member/memberLoginForm";
+    }
+
 }
